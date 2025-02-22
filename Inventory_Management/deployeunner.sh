fb0748b74f900bd18e2c1285b57e36091ad1a974
suresh <suresh.luvguitar@gmail.com>
2025-02-22 16:22:03 +0000

    first commit

---
+++ Inventory_Management/deployeunner.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+DOCKER_DEFAULT_PLATFORM="linux/amd64" docker build -t inventory_management .
+docker tag inventory_management sureshk89/inventory_managemente:1.0.0
+docker push sureshk89/inventory_management:1.0.0
+kubectl apply -f deployment.yaml
